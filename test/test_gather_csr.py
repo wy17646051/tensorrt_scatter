8215e1c88ff30f5ddc90d543526ec03cfb1491d0
wy17646051 <wy17646051@hotmail.com>
2023-05-03 14:28:29 +0800

    test: add test logic of GatherCSRPlugin

--- test/test_gather_csr.py
+++ test/test_gather_csr.py
@@ -9,40 +9,40 @@ from torch.onnx import register_custom_op_symbolic
 
 import testing
 from example.script import symbolic
-from example.script.model import GatherCOOExample
+from example.script.model import GatherCSRExample
 
-register_custom_op_symbolic(f'torch_scatter::gather_coo', symbolic.gather_coo, 11)
+register_custom_op_symbolic(f'torch_scatter::gather_csr', symbolic.gather_csr, 11)
 
 
 cases_general = [
     (
         [1, 2, 3, 4],             #  src
-        [0, 0, 1, 1, 1, 3],       #  index
+        [0, 2, 5, 5, 6],          #  indptr
         [-2, -2, -2, -2, -2, -2], #  base
     ),
     (
         [[1, 2], [3, 4], [5, 6], [7, 8]],                            #  src
-        [0, 0, 1, 1, 1, 3],                                          #  index
+        [0, 2, 5, 5, 6],                                             #  indptr
         [[-2, -2], [-2, -2], [-2, -2], [-2, -2], [-2, -2], [-2, -2]] #  base
     ),
     (
         [[1, 3, 5, 7], [2, 4, 6, 8]],                         #  src
-        [[0, 0, 1, 1, 1, 3], [0, 0, 0, 1, 1, 2]],             #  index
+        [[0, 2, 5, 5, 6], [0, 3, 5, 6, 6]],                   #  indptr
         [[-2, -2, -2, -2, -2, -2], [-2, -2, -2, -2, -2, -2]], #  base
     ),
     (
         [[[1, 2], [3, 4], [5, 6]], [[7, 9], [10, 11], [12, 13]]],         #  src
-        [[0, 0, 1], [0, 2, 2]],                                           #  index
+        [[0, 2, 3, 3], [0, 1, 1, 3]],                                     #  indptr
         [[[-2, -2], [-2, -2], [-2, -2]], [[-2, -2], [-2, -2], [-2, -2]]], #  base
     ),
     (
         [[1], [2]],           #  src
-        [[0, 0], [0, 0]],     #  index
+        [[0, 2], [0, 2]],     #  indptr
         [[-2, -2], [-2, -2]], #  base
     ),
     (
         [[[1, 1]], [[2, 2]]],                         #  src
-        [[0, 0], [0, 0]],                             #  index
+        [[0, 2], [0, 2]],                             #  indptr
         [[[-2, -2], [-2, -2]], [[-2, -2], [-2, -2]]], #  base
     )
 ]
@@ -51,58 +51,53 @@ cases_dtype = [torch.float32, torch.half]
 
 
 @pytest.mark.parametrize(
-    'src,index,base,with_base,dtype', 
+    'src,indptr,base,with_base,dtype', 
     testing.case_composite(cases_general,cases_with_base, cases_dtype)
 )
-def test_segment_coo_launth(src, index, base, with_base, dtype):
+def test_gather_csr_launth(src, indptr, base, with_base, dtype):
     src = torch.tensor(src, dtype=dtype).cuda()
-    index = torch.tensor(index, dtype=torch.int64).cuda()
+    indptr = torch.tensor(indptr, dtype=torch.int64).cuda()
     base = torch.tensor(base, dtype=dtype).cuda() if with_base else None
     
-    except_out = torch_scatter.gather_coo(src, index, base.clone() if base is not None else base)
+    except_out = torch_scatter.gather_csr(src, indptr, base.clone() if base is not None else base)
 
     # The `expand` operation in pytorch_scatter is implemented by converting the ONNX `Expand` operator, and the 
-    # gather_coo_lauch function does not implement related logic.
+    # gather_csr_lauch function does not implement related logic.
     # TensorRT Plugin or ONNX does not need to consider `contiguous` operations, so no relevant logic is designed in 
     # the scatter_lauch function.
-    index = index.expand(*src.shape[:index.dim()-1], index.shape[-1]).contiguous()
+    indptr = indptr.expand(*src.shape[:indptr.dim()-1], indptr.shape[-1]).contiguous()
 
-    out = src.new_empty([s if i != index.dim()-1 else index.shape[-1] for i, s in enumerate(src.shape)])
-    torch.ops.tensorrt_scatter.gather_coo_ptlaunth(src, index, base, out)
+    out = src.new_empty([s if i != indptr.dim()-1 else max(indptr.flatten()[-1], 0) for i, s in enumerate(src.shape)])
+    torch.ops.tensorrt_scatter.gather_csr_ptlaunth(src, indptr, base, out)
 
     assert torch.allclose(out, except_out)
 
 
 @pytest.mark.parametrize(
-    'src,index,base,with_base,dtype', 
-    testing.case_composite(cases_general,cases_with_base, cases_dtype)
+    'src,indptr,base,dtype', 
+    testing.case_composite(cases_general, cases_dtype)
 )
-def test_gather_coo_plugin(src, index, base, with_base, dtype):
+def test_gather_csr_plugin(src, indptr, base, dtype):
     with tempfile.TemporaryDirectory() as temp_dir:
         src = torch.tensor(src, dtype=dtype).cuda()
-        index = torch.tensor(index, dtype=torch.int64).cuda()
-        base = torch.tensor(base, dtype=dtype).cuda() if with_base else None
-        model = GatherCOOExample().cuda()
+        indptr = torch.tensor(indptr, dtype=torch.int64).cuda()
+        base = torch.tensor(base, dtype=dtype).cuda()
+        model = GatherCSRExample().cuda()
 
         with torch.no_grad():
-            except_out = model(src, index, base.clone() if with_base else None)
+            except_out = model(src, indptr, base)
             except_out = except_out.cpu().numpy()
         
-        _base = base.clone() if with_base else None
         testing.export_test_onnx(temp_dir, model, 
-                                 (src, index, _base) if with_base else (src, index),
-                                 ['src', 'index', 'base'] if with_base else ['src', 'index'],
+                                 (src, indptr, base),
+                                 ['src', 'indptr', 'base'],
                                  ['out'])
 
         trt_logger = trt.Logger(trt.Logger.WARNING)
         testing.export_test_trt(temp_dir, trt_logger)        
 
-        inputs = [src.cpu().numpy(), index.cpu().numpy().astype(np.int32)]
-        if with_base:
-            inputs.append(base.cpu().numpy())
-        dim = index.dim() - 1        
-        out_shape = [s if i != dim else index.shape[dim] for i, s in enumerate(src.shape)]
-        outputs = [np.empty(out_shape, dtype=inputs[0].dtype)]
+        inputs = [src.cpu().numpy(), indptr.cpu().numpy().astype(np.int32), base.cpu().numpy()]
+        outputs = [np.empty(base.shape, dtype=inputs[0].dtype)]
 
         out = testing.execute_test_trt(temp_dir, trt_logger, inputs, outputs)[0]
 
